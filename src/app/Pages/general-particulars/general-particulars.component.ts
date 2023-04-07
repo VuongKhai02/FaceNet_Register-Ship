d621af3643bddc30d9182db1c316f48aec2c486b
ZUKINOFTW <nguyentienloi04042001@gmail.com>
2023-04-07 17:54:16 +0700

    update

--- src/app/Pages/general-particulars/general-particulars.component.ts
+++ src/app/Pages/general-particulars/general-particulars.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-general-particulars',
   templateUrl: './general-particulars.component.html',
-  styleUrls: ['./general-particulars.component.css']
+  styleUrls: ['./general-particulars.component.css'],
 })
-export class GeneralParticularsComponent {
-
-}
+export class GeneralParticularsComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Tm1Component } from './Pages/tm1/tm1.component';
 import { Tm2Component } from './Pages/tm2/tm2.component';
+import { Tm3Component } from './Pages/tm3/tm3.component';
+import { Tm4Component } from './Pages/tm4/tm4.component';
+import { Tm5Component } from './Pages/tm5/tm5.component';
+import { Tm6Component } from './Pages/tm6/tm6.component';
+import { Tm7Component } from './Pages/tm7/tm7.component';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { vi_VN } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
@@ -15,14 +20,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IconsProviderModule } from './icons-provider.module';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
-
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { GeneralParticularsComponent } from './Pages/general-particulars/general-particulars.component';
+import { AccountManagementComponent } from './Pages/account-management/account-management.component';
 registerLocaleData(vi);
 
 @NgModule({
   declarations: [
     AppComponent,
     Tm1Component,
-    Tm2Component
+    Tm2Component,
+    Tm3Component,
+    Tm4Component,
+    Tm5Component,
+    Tm6Component,
+    Tm7Component,
+    AccountManagementComponent,
+    GeneralParticularsComponent,
   ],
   imports: [
     BrowserModule,
@@ -32,11 +46,10 @@ registerLocaleData(vi);
     BrowserAnimationsModule,
     IconsProviderModule,
     NzLayoutModule,
-    NzMenuModule
+    NzMenuModule,
+    NzFormModule,
   ],
-  providers: [
-    { provide: NZ_I18N, useValue: vi_VN }
-  ],
-  bootstrap: [AppComponent]
+  providers: [{ provide: NZ_I18N, useValue: vi_VN }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
