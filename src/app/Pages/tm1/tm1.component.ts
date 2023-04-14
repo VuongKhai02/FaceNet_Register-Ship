cb9712389b7f2ec14a113d13676087df84a52582
tule489 <leminhtu489@gmail.com>
2023-04-14 13:37:33 +0700

    Update tm1, tm2, tm3

--- src/app/Pages/tm1/tm1.component.ts
+++ src/app/Pages/tm1/tm1.component.ts
@@ -1,65 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
-interface ItemData {
-  id: string;
-  platePosition: string;
-  noOrLetter: string;
-  originalThickness: string;
-  forwardReadingP: string;
-  forwardReadingS: string;
-  afterReadingP: string;
-  afterReadingS: string;
-  meanDiminutionP: string;
-  meanDiminutionS: string;
-}
-
 @Component({
   selector: 'app-tm1',
   templateUrl: './tm1.component.html',
   styleUrls: ['./tm1.component.css'],
 })
 export class Tm1Component implements OnInit {
-  strakePosition: string = 'TEST';
-  isEditStrakePosition: boolean = false;
-  i = 0;
-  editId: string | null = null;
-  listOfData: ItemData[] = [];
+  addRowValue: number = 0;
+  rowValue: number[] = [];
 
-  startEdit(id: string): void {
-    this.editId = id;
+  ngOnInit(): void {
+    for (let i = 1; i <= 20; i++) this.rowValue.push(i);
   }
-
-  startEditStrakePosition(): void {
-    this.isEditStrakePosition = true;
+  addRow() {
+    for (let i = 1; i <= this.addRowValue; i++)
+      this.rowValue.push(this.rowValue[this.rowValue.length - 1] + 1);
   }
-
-  stopEdit(): void {
-    this.editId = null;
-    this.isEditStrakePosition = false;
-  }
-
-  addRow(): void {
-    this.listOfData = [
-      ...this.listOfData,
-      {
-        id: `${this.i}`,
-        platePosition: '',
-        noOrLetter: '',
-        originalThickness: '',
-        forwardReadingP: '',
-        forwardReadingS: '',
-        afterReadingP: '',
-        afterReadingS: '',
-        meanDiminutionP: '',
-        meanDiminutionS: '',
-      },
-    ];
-    this.i++;
-  }
-
-  deleteRow(id: string): void {
-    this.listOfData = this.listOfData.filter((d) => d.id !== id);
-  }
-
-  ngOnInit(): void {}
 }
