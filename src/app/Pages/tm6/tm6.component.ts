9dedd37c7b7cee97e1ae095749af2d6811d4ca23
codermyl <87702634+codermyl@users.noreply.github.com>
2023-04-15 15:20:59 +0700

    update

--- src/app/Pages/tm6/tm6.component.ts
+++ src/app/Pages/tm6/tm6.component.ts
@@ -5,15 +5,29 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './tm6.component.html',
   styleUrls: ['./tm6.component.css']
 })
+
 export class Tm6Component implements OnInit {
   addRowValue: number = 0;
-  rowValue: number[] = [];
+  rowValue: TM6[] = [];
   ngOnInit(): void {
     for(let i = 1; i <= 20; i++)
-      this.rowValue.push(i);
+      this.rowValue.push({desc:'',orgThk:'',maxAlw:'',gauP:'',gauS:''});
   }
   addRow(){
     for(let i = 1; i <= this.addRowValue; i++)
-      this.rowValue.push(this.rowValue[this.rowValue.length-1]+1);
+      this.rowValue.push({desc:'',orgThk:'',maxAlw:'',gauP:'',gauS:''});
   }
+
+  toNumber(x: string){
+    return (Number)(x);
+  }
+
+}
+
+interface TM6{
+  desc: string;
+  orgThk:  string;
+  maxAlw:  string;
+  gauP:  string;
+  gauS:  string;
 }
